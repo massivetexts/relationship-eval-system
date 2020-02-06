8b6fb5d3aabf5571e679ddfb0da23926273b1072
Peter Organisciak <organisciak@gmail.com>
2020-02-06 14:48:02 -0700

    Add downloading and file optimization

--- collector.py
+++ collector.py
@@ -1,13 +1,14 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, Response
 import pandas as pd
 import json
 from htrc_features import utils
 import time
 import glob
+import os
 
 app = Flask(__name__)
 
-def css_escape(htid): 
+def css_escape(htid):
     return (utils.clean_htid(htid).replace('.', '-A')
             .replace('$', '-B').replace(',', '-C')
             .replace('+', '-D').replace('=', '-E')
@@ -25,27 +26,62 @@ def css_unescape(htid):
     return id_decode(unescaped)
 
 def get_htid_list():
-    files = glob.glob(app.root_path + '/data/*')
-    clean_htids = [file.split('data/')[1].replace('.json', '') for file in files]
+    files = glob.glob(os.path.join(app.root_path, 'data', '*'))
+    clean_htids = [os.path.split(file)[1].replace('.json', '') for file in files]
+
     return [id_decode(htid) for htid in clean_htids]
+
+@app.route("/optimize")
+def optimize(pathglob='results/results.*.csv'):
+    df, rater_targets = load_results()
+
+    old_len = df.shape[0]
+    df = df.drop_duplicates(['rater', 'target', 'candidate', 'judgment'])
+    new_len = df.shape[0]
+
+    old_files = glob.glob(os.path.join(app.root_path, pathglob))
+    response = "<h2> Rewriting data files</h2><br />"
+    prefix = 'old.'+str(int(time.time()))+'.'
+    for old_file in old_files:
+        path, filename = os.path.split(old_file)
+        new_file = os.path.join(path, prefix + filename)
+        os.rename(old_file, new_file)
+
+    df.to_csv(os.path.join(app.root_path, 'results', 'results.0.csv'), mode='a', header=False)
+
+    response += "Original size: {}, Deduplicated size: {}".format(old_len, new_len)
+    return response
+
+@app.route("/download")
+def getCSV():
+    optimize()
+    df, rater_targets = load_results()
+    df = df.drop_duplicates(['rater', 'target', 'candidate', 'judgment'])
+    csv = df.to_csv()
+
+    return Response(
+        csv,
+        mimetype="text/csv",
+        headers={"Content-disposition":
+                 "attachment; filename=ratings.csv"})
 
 def request_as_df(batch):
     rows = []
-    timestamp = time.time() 
+    timestamp = time.time()
     candidates = [key for key in batch.keys() if key.startswith('judgment')]
 
     for field in ['name', 'targetId']:
         if field not in batch:
                 batch[field] = None
-   
+
     for candidate in candidates:
         chtid = css_unescape(candidate.replace('judgment-', ''))
         notesid = candidate.replace('judgment-', 'notes-')
         if notesid not in batch:
             batch[notesid] = None
-        row = { 'rater': batch['name'], 
-               'target': batch['targetId'], 
-               'candidate': chtid, 
+        row = { 'rater': batch['name'],
+               'target': batch['targetId'],
+               'candidate': chtid,
                'judgment': batch[candidate],
                'notes': batch[notesid],
                'timestamp': timestamp
@@ -80,14 +116,14 @@ def print_target_counts(rater_targets, rater=None, n=1000):
         target_counts = target_counts[~target_counts.index.isin(done_by_rater)]
 
     li = "<ol>"
-    for htid,n in target_counts.iloc[:n].iteritems(): 
+    for htid,n in target_counts.iloc[:n].iteritems():
         li += "<li><a href='{}'>{}</a> - Times rated: {}</li>".format(rating_url(htid, rater=rater), htid, n)
     li += "</ol>"
     return li
 
 def print_recently_rated(rater_targets, n=5):
     return rater_targets.sort_values('timestamp', ascending=False).head(n).to_html()
-    
+
 def load_results(pathglob='results/results.*.csv', drop_duplicates=True):
     colnames = ['rater', 'target', 'candidate', 'judgment', 'notes', 'timestamp']
     try:
@@ -99,7 +135,7 @@ def load_results(pathglob='results/results.*.csv', drop_duplicates=True):
         df = df.drop_duplicates()
     rater_targets = df[['rater', 'target', 'timestamp']].drop_duplicates(['rater', 'target'])
     return (df, rater_targets)
-    
+
 def global_stats():
     pass
 
@@ -114,10 +150,13 @@ def submit():
     input = dict(request.args.items())
     name = request.args.get('name')
     response = ""
+    if not os.path.exists(os.path.join(app.root_path, 'results')):
+        os.mkdir(os.path.join(app.root_path, 'results'))
+
     if len(input) > 1:
         try:
             formatted = request_as_df(input)
-            formatted.to_csv(app.root_path + '/results/results.0.csv', mode='a', header=False)
+            formatted.to_csv(os.path.join(app.root_path, 'results', 'results.0.csv'), mode='a', header=False)
             response += "Submitted! Below is the data you submitted. "
             response += "If you need something fixed later, you can save this for reference:<hr />"
         except IOError:
@@ -126,38 +165,38 @@ def submit():
             response += "<h1>Nothing submitted due to error.</h1>If unexpected,"
             response += "</h1>Save this string somewhere for Dr. O to inspect:<br/>"
         response += "<textarea>{}</textarea>".format(input)
-        
-    df, rater_targets = load_results()  
-        
+
+    df, rater_targets = load_results()
+
     response += "<hr /><h2>More to rate</h2>"
     response += print_target_counts(rater_targets, rater=name, n=15)
-    
+
     response += "<hr /><h2>How much have you rated?</h2>"
     response += print_rater_counts(rater_targets)
 
     response += "<hr /><h2>Recently Rated</h2>"
     response += print_recently_rated(rater_targets)
-    
+
     return response
 
 @app.route("/")
 def stats():
     name = request.args.get('name')
     response = "<h1>Info</h1>"
-    
+
     df, rater_targets = load_results()
-    
+
     response += "<hr /><h2>More to rate</h2>"
     response += print_target_counts(rater_targets, rater=name, n=15)
-    
+
     response += "<hr /><h2>How much have you rated?</h2>"
     response += print_rater_counts(rater_targets)
 
     response += "<hr /><h2>Recently Rated</h2>"
     response += print_recently_rated(rater_targets)
-    
+
     return response
-    
+
 
 @app.route("/rating")
 def rating():
@@ -174,6 +213,6 @@ def send_js(path):
 @app.route('/css/<path:path>')
 def send_css(path):
     return send_from_directory('static/css', path)
-    
+
 if __name__ == "__main__":
     app.run(debug=True)
